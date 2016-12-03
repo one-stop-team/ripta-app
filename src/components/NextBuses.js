6cc5290bd11d750c7b77e7f805468ded0933cf1d
John Bennett <john@jtbennett.com>
2016-12-03 11:44:08 -0500

    Call api to get tripupdates and stops

--- src/components/NextBuses.js
+++ src/components/NextBuses.js
@@ -4,16 +4,33 @@ import { Col, Row } from 'react-bootstrap'
 import logo from '../images/ripta_logo.png'
 
 export default class NextBuses extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      stops: [],
+      tripUpdates: []
+    }
+  }
 
   componentDidMount() {
     fetch('http://localhost:8000/api/tripupdates/route/34')
-      .then(response => {
-        console.log(response)
+      .then(response => response.json())
+      .then(json => {
+        this.setState({ tripUpdates: json.entity })
       })
+
+    fetch('http://localhost:8000/api/route/34/stops')
+      .then(response => response.json())
+      .then(json => {
+        this.setState({ stops: json })
+      })
+
   }
 
   render() {
     const { bus } = this.props
+    console.log('tripUpdates', this.state.tripUpdates)
+    console.log('stops', this.state.stops)
     return (
       <Row>
         <Col xs={12}>
