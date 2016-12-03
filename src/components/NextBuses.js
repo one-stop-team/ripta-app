ce48d1857eff1f371262436be6b98c6f1526f953
John Bennett <john@jtbennett.com>
2016-12-03 12:59:16 -0500

    Work in progress on next buses

--- src/components/NextBuses.js
+++ src/components/NextBuses.js
@@ -7,6 +7,7 @@ export default class NextBuses extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
+      stopId: "13705",
       stops: [],
       tripUpdates: []
     }
@@ -27,15 +28,47 @@ export default class NextBuses extends React.Component {
 
   }
 
+  getStopTimeUpdates() {
+    const buses = this.state.tripUpdates.map((tripUpdate) => {
+      const stopUpdate = tripUpdate.trip_update.stop_time_update
+        .find(item => item.stop_id === this.state.stopId)
+      return stopUpdate
+    })
+    return buses
+  }
+
+  getStopName(id) {
+    const stop = this.state.stops.find(
+      stop => stop.stop_id === this.state.stopId)
+    return stop ? stop.stop_name : ''
+  }
+
   render() {
-    const { bus } = this.props
+    const stopTimeUpdates = this.getStopTimeUpdates()
     console.log('tripUpdates', this.state.tripUpdates)
+    console.log('stopTimeUpdates', stopTimeUpdates)
     console.log('stops', this.state.stops)
+
+    const stopName = this.getStopName(this.state.stopId)
+
     return (
       <Row>
         <Col xs={12}>
+          <b>{this.state.stopId + ': ' + stopName}</b>
+          <ul>
+            {stopTimeUpdates.map(update => {
+              return <li>foo</li>
+            }
+            )}
+          </ul>
         </Col>
       </Row>
     )
   }
 }
+
+//  if (update && update.arrival) {
+//                 return <li>{`${update.arrival.time}: (delayed ${update.arrival.delay} seconds)`}</li>
+//               } else {
+//                 return null
+//               }
