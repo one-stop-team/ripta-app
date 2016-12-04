010df434deffeca35e7b28101844458deccd603d
JD Sareault <jdsareault@gmail.com>
2016-12-04 14:36:08 -0500

    Add Time To Next Bus to update within HTML

--- src/App.js
+++ src/App.js
@@ -19,47 +19,6 @@ class App extends Component {
             <NextBuses />
           </bs.Grid>
 
-          <div className="panel-heading">
-            <h3 className="panel-title">Sunday, December 4, 2016</h3>
-          </div>
-          <p>Last Update: 6:30 p.m.</p>
-          <div className="panel panel-success">
-            <div className="panel-heading">
-              <span className="panel-title">
-                <span className="label label-primary">
-                  <i className="fa fa-bus" aria-hidden="true"></i> 17
-                </span> #675
-              </span>
-              <span className="badge pull-right">On time</span>
-            </div>
-            <div className="container-fluid">
-              <div className="row">
-              <div className="col-sm-4">
-                  <i className="fa fa-bus" aria-hidden="true"></i>
-                  <b>Stop</b>
-                  <br></br>
-                  Hope Street
-                  <br></br>
-                  7:00 p.m.
-                </div>
-              <div className="col-sm-4">
-                  <i className="fa fa-road" aria-hidden="true"></i>
-                  <b>Travel duration</b>
-                  <br></br>
-                  20 minutes
-                </div>
-              <div className="col-sm-4">
-                  <i className="fa fa-map-marker" aria-hidden="true"></i>
-                  <b>Destination</b>
-                  <br></br>
-                  Kennedy Plaza
-                  <br></br>
-                  7:20 p.m.
-                </div>
-              </div>
-            </div>
-          </div>
-
           <div className="panel panel-danger">
             <div className="panel-heading">
               <span className="panel-title">
@@ -73,11 +32,9 @@ class App extends Component {
               <div className="row">
               <div className="col-sm-4">
                   <i className="fa fa-bus" aria-hidden="true"></i>
-                  <b>Stop</b>
+                  <b> Time Until Next Bus</b>
                   <br></br>
-                  Place Providence
-                  <br></br>
-                  7:05 p.m.
+                  45 minutes
                 </div>
               <div className="col-sm-4">
                   <i className="fa fa-road" aria-hidden="true"></i>
